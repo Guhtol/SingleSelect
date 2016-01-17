b81dbede3c647178d89fe3cab72fff499decba6b
Guhtol <bueno_oab@hotmail.com>
2016-01-17 20:41:25 -0200

    Insert a exemplo c# that consume this

---
+++ Exemplo-C#/SingleSelect/Models/Cidade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingleSelect.Models
+{
+    public class Cidade
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
---
+++ Exemplo-C#/SingleSelect/Scripts/Cidades/Cidades.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(init);
+
+var select = Select('selectId');
+
+function init() {
+  
+    $.get('Cidades/ListaCidades', ajaxResponse);
+};
+
+function ajaxResponse(data) {
+
+    select.append(data,'Nome','Id');
+}
