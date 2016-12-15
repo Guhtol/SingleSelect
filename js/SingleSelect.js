3c9f965e67afbd00550f86420cfd15a90b44f1cb
BuenoOtavio <programador.bueno@gmail.com>
2016-12-15 14:37:42 -0200

    Alteração para Programação Funcional

--- js/SingleSelect.js
+++ js/SingleSelect.js
@@ -1,199 +1,83 @@
-var SgSelect = (function () {
-    var option = {},
-        opFragment = {},
-        textSelected = [],
-        valueSelected = [],
-        arrayPropertyWrong = [];
+(function (root) {
+    'use strict'
 
+    return root.sgSelect = {
+        init: init,
+        getValue: getSelectedValue,
+        getText: getSelectedText
+    }
 
-    var len,
-        i,
-        checkFirst = true;
+    function init(select) {
+        return function (value) {
+            return function (text) {
+                return function (obj) { 
+                    var fragment = document.createDocumentFragment('option')                   
+                    if (typeof select === "string")
+                        select = document.getElementById(select)
 
+                    if (select === null)
+                        throw 'Verifique o seletorId'
 
-    InnerSelect.prototype.append = append;
+                    select.appendChild(createFragment(fragment,obj, value, text))
 
-    InnerSelect.prototype.selectItem = selectItem;
-
-    InnerSelect.prototype.defaultItem = defaultItem;
-
-    InnerSelect.prototype.reset = resetSelect;
-
-    return InnerSelect;
-
-    function InnerSelect(selectId) {
-
-        if (this instanceof InnerSelect) {
-
-            this.selectHtml = document.getElementById(selectId);
-
-            this.text = getSelectedText;
-
-            this.value = getSelectedValue;
-
-        } else {
-
-            return new InnerSelect(selectId);
-        }
-    };
-
-    function getSelectedText() {
-
-        textSelected = [];
-
-        if (this.selectHtml.multiple) {
-
-            resetWhile(this.selectHtml.selectedOptions);
-
-            while (i < len) {
-
-                textSelected.push(this.selectHtml.selectedOptions[i].text);
-
-                i++;
+                    return select;
+                }
             }
+        }
+    }
 
-            return textSelected;
-
+    function createFragment(fragment,obj, value, text) {        
+        if (isArray(obj)) {
+            obj.map(function (item) {
+                if (!checkProperty(item, value) || !checkProperty(item, text))
+                    throw 'Verifique os nomes das propriedades'
+                fragment.appendChild(createOption(item, value, text))
+            })
         } else {
-
-            return this.selectHtml.options[this.selectHtml.selectedIndex].text;
+            if (!checkProperty(obj, value) || !checkProperty(obj, text))
+                throw 'Verifique os nomes das propriedades'
+            fragment.appendChild(createOption(obj, value, text))
         }
+        return fragment;
+    }
 
-    };
-    function getSelectedValue() {
-
-        valueSelected = [];
-
-        if (this.selectHtml.multiple) {
-
-            resetWhile(this.selectHtml.selectedOptions);
-
-            while (i < len) {
-
-                valueSelected.push(this.selectHtml.selectedOptions[i].value);
-
-                i++;
-            }
-
-            return valueSelected;
-
-        } else {
-
-            return this.selectHtml.options[this.selectHtml.selectedIndex].value;
-        }
-    };
-
-    function append(obj, text, value) {
-
-        //here  set the default value to run while
-        resetWhile(obj);
-
-        opFragment = document.createDocumentFragment('option');
-
-        // if obj it's not array 
-        if (len === undefined) {
-
-            createOption(obj, text, value);
-        }
-        // if obj is array
-        while (i < len) {
-
-            createOptionArray(obj, text, value, i);
-
-            i++;
-        }
-
-        this.selectHtml.appendChild(opFragment);
-
-    };
-
-    function selectItem(item) {
-
-        this.selectHtml.value = item;
-
-    };
-    function defaultItem(item) {
-
-        option = document.createElement('option');
-
-        option.textContent = item;
-
-        option.value = "";
-
-        this.selectHtml.appendChild(option);
-
-    };
-    function resetSelect() {
-        
-        this.selectHtml.innerHTML = "";
-    };
-
-    function createOption(obj, text, value) {
-
-        isProperty(obj, text, value)
-
-        option = document.createElement('option');
+    function createOption(obj, value, text) {
+        var option = document.createElement('option')
 
         option.textContent = obj[text].toString();
 
         option.value = obj[value].toString();
 
-        opFragment.appendChild(option);
-    };
+        return option;
+    }
 
-    function createOptionArray(obj, text, value) {
+    function checkProperty(obj, value) {
+        return obj.hasOwnProperty(value)
+    }
 
-        //check if have property text and value in object;
-        checkFirstObject(obj, text, value);
-
-        option = document.createElement('option');
-
-        option.textContent = obj[i][text].toString();
-
-        option.value = obj[i][value].toString();
-
-        opFragment.appendChild(option);
-    };
-
-    function checkFirstObject(obj, text, value) {
-
-        //check once
-        if (checkFirst) {
-
-            checkFirst = false;
-
-            isProperty(obj[i], text, value);
+    function getSelectedValue(obj) {
+        if (obj.multiple) {
+            return obj.selectedOptions.map(multiplevalues)
         }
-    };
-    
-    function isProperty(obj, text, value) {
+        return obj.options[obj.selectedIndex].value;
+    }
 
-        arrayPropertyWrong = [];
-
-        if (typeof obj === 'object') {
-
-            if (!obj.hasOwnProperty(text)) {
-
-                arrayPropertyWrong.push(text);
-            }
-            if (!obj.hasOwnProperty(value)) {
-
-                arrayPropertyWrong.push(value);
-            }
-            if (arrayPropertyWrong.length > 0) {
-
-                throw new Error('object not have this property = ' + arrayPropertyWrong.join(','));
-            }
-        } else {
-
-            throw new Error('it is not a object');
+    function getSelectedText(obj) {
+        if (obj.multiple) {
+            return obj.selectedOptions.map(getText)
         }
-    };
+        return obj.options[obj.selectedIndex].text;
+    }
 
-    function resetWhile(obj) {
+    function getValue(obj) {
+        return obj.value;
+    }
 
-        len = obj.length;
+    function getText(obj) {
+        return obj.text;
+    }
 
-        i = 0;
-    };
-})();
+    function isArray(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]';
+    }
+} (this))
